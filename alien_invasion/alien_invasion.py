efc16ea9f9001ebf15e9a1780a226fb1f62a6b84
Max MacDonald <mmacdon@tcd.ie>
2020-10-23 22:31:25 +0200

    alien_invasion piloting the ship

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -15,11 +15,12 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     
     # Make a ship.
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # Start the main loop for the game.
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
         # Make the most recently drawn screen visible.
