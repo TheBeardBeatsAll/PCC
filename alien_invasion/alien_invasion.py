229e6d9ea1a95779c8f55b7a47f3a53e6f87ee28
Max MacDonald <mmacdon@tcd.ie>
2020-10-23 23:04:34 +0200

    alien_invasion shooting bullets

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,8 +1,10 @@
 import sys
 import pygame
 import game_functions as gf
+
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
 
 
 def run_game():
@@ -16,13 +18,14 @@ def run_game():
     
     # Make a ship.
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
 run_game()
