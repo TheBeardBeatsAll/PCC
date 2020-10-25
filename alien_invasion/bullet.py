2f01a3ba793570fb111dfb0bc7e9fc8ec563744f
Max MacDonald <mmacdon@tcd.ie>
2020-10-25 14:26:40 +0100

    alien_invasion making the fleet move

--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -8,11 +8,13 @@ class Bullet(Sprite):
         """Create a bullet object at the ship's current position."""
         super().__init__()
         self.screen = screen
+
         # Create a bullet rect at (0, 0) and then set correct position.
         self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,
         ai_settings.bullet_height)
         self.rect.centerx = ship.rect.centerx
         self.rect.top = ship.rect.top
+
         # Store the bullet's position as a decimal value.
         self.y = float(self.rect.y)
         self.color = ai_settings.bullet_color
@@ -22,6 +24,7 @@ class Bullet(Sprite):
         """Move the bullet up the screen."""
         # Update the decimal position of the bullet.
         self.y -= self.speed_factor
+        
         # Update the rect position.
         self.rect.y = self.y
     
