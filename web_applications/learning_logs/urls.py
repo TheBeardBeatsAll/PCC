146fddbf025f02badc48094fbb10a9d45a48b401
Max MacDonald <mmacdon@tcd.ie>
2020-10-26 15:06:59 +0100

    web_applications building additional pages

--- web_applications/learning_logs/urls.py
+++ web_applications/learning_logs/urls.py
@@ -1,8 +1,12 @@
 """Defines URL patterns for learning_logs."""
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
     # Home page
-    url(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
